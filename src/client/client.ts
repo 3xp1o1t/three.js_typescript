ada614c0854338408ac7dbced5e764371217588d
3xp1o1t <jes.monti.503@gmail.com>
2023-08-23 18:35:50 -0600

    [Added] Gui for status and control props
    Se agrego una gui para mostrar el status de la escena
    Muestra los FPS/MS/MB.
    Se agrego una gui para modificar en tiempo real las propiedades
    de los ejes x, y & z de el cubo en escena y la camara.

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,7 +1,14 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+// Estadisticas (fps/ ms / size)
+import Stats from "three/examples/jsm/libs/stats.module";
+// GUI para manipular las propiedades de la camara y el cubo visualmente.
+import { GUI } from "dat.gui";
+
 // 1er elemento - La scena es un plano 3D (X, Y, Z)
 const scene = new THREE.Scene();
+// Agregar lineas para los ejex X,Y,Z de grosor 5
+scene.add(new THREE.AxesHelper(5));
 
 // 2do elemento - La camara es el foco para previsualizar la escena.
 const camera = new THREE.PerspectiveCamera(
@@ -18,7 +25,8 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-new OrbitControls(camera, renderer.domElement);
+// new OrbitControls(camera, renderer.domElement);
+const controls = new OrbitControls(camera, renderer.domElement);
 
 // componentes de una forma
 const geometry = new THREE.BoxGeometry();
@@ -40,13 +48,48 @@ function onWindowResize() {
   render();
 }
 
+// Agregar el objeto Stats para renderizar un div
+// con click izq cambiamos el tipo de estadistica.
+const stats = new Stats();
+document.body.appendChild(stats.dom);
+
+// Agregamos la interfaz visual para manipular las props del cubo.
+const gui = new GUI();
+// Folder para operar con la rotacion del cubo
+const cubeFolderRotation = gui.addFolder("Cube Rotation");
+const cubeFolderScale = gui.addFolder("Cube Scale");
+const cubeFolderPosition = gui.addFolder("Cube Position");
+// Agregar las propiedades del cubo (Rotacion)
+cubeFolderRotation.add(cube.rotation, "x", 0, Math.PI * 2);
+cubeFolderRotation.add(cube.rotation, "y", 0, Math.PI * 2);
+cubeFolderRotation.add(cube.rotation, "z", 0, Math.PI * 2);
+
+// Propiedades de Posicion.
+cubeFolderPosition.add(cube.position, "x", -10, 10, 1);
+cubeFolderPosition.add(cube.position, "y", -10, 10, 1);
+cubeFolderPosition.add(cube.position, "z", -10, 10, 1);
+
+// Propiedades Scala
+cubeFolderScale.add(cube.scale, "x", -5.0, 5.0);
+cubeFolderScale.add(cube.scale, "y", -5.0, 5.0);
+cubeFolderScale.add(cube.scale, "z", -5.0, 5.0);
+
+cubeFolderRotation.open(); // Abrir el gui.
+cubeFolderScale.open();
+cubeFolderPosition.open();
+
+// camera
+const cameraFolder = gui.addFolder("Camera");
+cameraFolder.add(camera.position, "z", 0, 10);
+cameraFolder.open();
+
 function animate() {
   requestAnimationFrame(animate);
 
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
-
   render();
+
+  // actulizamos los stats.
+  stats.update();
 }
 
 function render() {
